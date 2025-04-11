4f0080bb0b043070f9a7dc9753effe652b3da109
Knezz009 <kacpernowicki9@gmail.com>
2025-04-11 20:18:10 +0200

    game.js

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 400;
-canvas.height = 600;
+canvas.width = 800;
+canvas.height = 1000;
 
 let cameraY = 0;
 let score = 0;
